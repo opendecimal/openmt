5f5bafb803978a36e380a451ec89db2066301407
hikelin <895351856@qq.com>
2025-10-21 17:38:19 +0800

    Add UserHeader

--- src/main/java/com/opendecimal/openmt/model/UserHeader.java
+++ src/main/java/com/opendecimal/openmt/model/UserHeader.java
@@ -1,10 +1,31 @@
 package com.opendecimal.openmt.model;
 
 public class UserHeader extends Block {
+
     public final static int NUM = 3;
+
+    private String bankingPriority;
+
+    private String messageUserReference;
 
     @Override
     public int getNum() {
         return NUM;
+    }
+
+    public String getBankingPriority() {
+        return bankingPriority;
+    }
+
+    public void setBankingPriority(String bankingPriority) {
+        this.bankingPriority = bankingPriority;
+    }
+
+    public String getMessageUserReference() {
+        return messageUserReference;
+    }
+
+    public void setMessageUserReference(String messageUserReference) {
+        this.messageUserReference = messageUserReference;
     }
 }
